a9e5be07b1bea405a90be667ea83100bd8edb508
Jiwei99 <jwlim99@gmail.com>
2022-10-13 23:35:42 +0800

    Style important dates component

--- components/ImportantDates/index.tsx
+++ components/ImportantDates/index.tsx
@@ -1,56 +1,37 @@
-import { Box } from "@chakra-ui/react";
+import React from "react";
+import { ListItem } from "@chakra-ui/react";
+import { ImportantDate } from "../../types/Dates";
+import { importantDates } from "../../data/ImportantDates";
+import { ListRow, RowDate, RowDesc, StyledUnorderedList, Title, Wrapper } from "./styles";
 
-/* export  function ImportantDates() {
-    
-    return (
-        <Box bg='tomato' w='100%' p={4} color='white'>
-        This is the Box
-      </Box>
-    )
-} */
-{/* <style>
-#rcorners {
-  borderRadius: 25px;
-  background: #73AD21;
-  padding: 20px;
-  width: 200px;
-  height: 150px;
-}
-</style> */}
+export default function ImportantDates() {
+    const dates: ImportantDate[] = importantDates;
 
-export type data={
-
-    date: string
-    event:string
-}
-
-const data: data[]=[
-    {
-        date:"20 dec",
-        event:"Workshop Registration"
-
-    },
-    {
-        date:"21 dec",
-        event:"Hackathon"
-
-
+    function datesComponent(dates: ImportantDate[]): React.ReactNode {
+        return dates.map((date) => {
+            return (
+                <ListItem key={date.id}>
+                    <ListRow>
+                        <RowDate>
+                            {`${date.date}:`}
+                        </RowDate>
+                        <RowDesc>
+                            {date.description}
+                        </RowDesc>
+                    </ListRow>
+                </ListItem>
+            )
+        })
     }
-]
 
-export function ImportantDates(data:data) {
-        
-    
     return (
-        
-
-//idk why div css is not applying         
-        <Box bg='white' w='400px' p={4}  borderRadius='2xl' h='400px' borderTopRadius="0" boxShadow='xl'>
-            <div color='red'>Important 
-            </div>
-        </Box>
+        <Wrapper>
+            <Title>
+                IMPORTANT DATES
+            </Title>
+            <StyledUnorderedList>
+                {datesComponent(dates)}
+            </StyledUnorderedList>
+        </Wrapper>
     )
 }
-
-data.map((item: data)=>ImportantDates(item))
-
---
+++ types/Dates.ts
@@ -0,0 +1,5 @@
+export type ImportantDate = {
+    id: number
+    date: string
+    description: string
+}
