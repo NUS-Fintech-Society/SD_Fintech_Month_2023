7f392fae6b7d328b7ce6453bdf10141784b12bae
Zheng Yu <zhengyu@Zhengs-MacBook-Air.local>
2022-10-21 11:42:36 +0800

    created and styles.tsx file and removed all inline styles

--- components/About/index.tsx
+++ components/About/index.tsx
@@ -1,43 +1,33 @@
-import {
-    Flex,
-    Container,
-    Box,
-    Text,
-    Heading,
-    Image
-} from '@chakra-ui/react'
-import { AboutContainer } from './styles'
+import {Text} from '@chakra-ui/react'
+import { AboutContainer, AboutHeading, HeadingContainer, ContentContainer, LeftContentContainer, FlushTop, Centralised, DescriptionBox, RightContentContainer, TopImage, MiddleImage, BottomImage } from './styles'
 
 const About = () => {
   return (
     <AboutContainer>
-        {/* header */}
-        <Flex w = '100%' h = '15%' align = 'center' justify={'center'} padding = '5%'>
-            <Heading fontSize = {{base : 'xl', md : '2xl'}}>ABOUT FINTECH MONTH</Heading>
-        </Flex>
-        <Flex w = '100%'  h ={'85%'} direction = {{base : 'column-reverse', md :'row'}} align = 'center'>
-            {/* left/bottom of page  */}
-            <Flex w = {{base:'80%', md:'65%'}} h = '100%' direction = 'column' align = 'center'>
-                <Flex height = '50%' align = 'center' justify= 'center' >
-                    <Flex align ='center' justify = 'center' >
-                        <Box flexShrink = 'none' w = '90%' display = 'flex-column' borderWidth='1px' borderRadius='20' padding= '20px 10px' boxShadow='0px 2px 1px grey' backdropFilter='blur(6px)'>
-                            <p>Donec pellentesque, ligula quis dictum lobortis, quam felis lacinia nulla, eget venenatis enim neque eget eros. Etiam auctor pret, tincidunt convallis quam. Quisque sit amet facilisis elit. </p>
-                            <p>Fusce semper neque metus, sit amet convallis lectus bibendum ut. Etiam lobortis dolor vitae turpis accumsan scelerisque.enean eu volutpat quam. Nam lacinia, risus id porttitor varius, mi ipsum hendrerit eros, non aliquam orci lorem quis lorem. Nunc semper venenatis rutrum. </p>
-                         </Box>
-                    </Flex>
-                </Flex>
-            </Flex>
-            {/* right/top of page */}
-            <Flex direction = 'column' align = 'center' justify = {{base :'center', md : 'start'}} w = {{base:'80%', md:'30%'}} h = {{base:'50%',md:'100%'}} padding = {{base : '20% 0', md :'0'}}>
-                <Flex h = '50%' w = '90%' align = 'center' justify= 'center' >
-                    <Image src='gibbresh.png' fallbackSrc='https://via.placeholder.com/150' boxSize='7rem' position = 'relative' zIndex='2' left = '6rem'
-                    right = {{base :'13rem', md :'13rem'}} bottom = {{base : '7rem',md : '4rem'}} borderRadius = '5px'/>
-                    <Image src='gibbresh.png' fallbackSrc='https://via.placeholder.com/150' boxSize = '12rem' position = 'relative' zIndex='1' 
-                    left = {{base :'2rem', md :'2rem'}} bottom ={{base :'1rem', md :'0'}} borderRadius = '5px'/> 
-                    <Image src='gibbresh.png' fallbackSrc='https://via.placeholder.com/150' boxSize='9rem' position = 'relative' zIndex='2' right = {{base :'13rem', md :'13rem'}} top = {{base : '2rem',md : '5rem'}} borderRadius = '5px'/>
-                </Flex>
-            </Flex>
-        </Flex>
+        <HeadingContainer>
+            <AboutHeading colorScheme = 'brand'>ABOUT FINTECH MONTH</AboutHeading>
+        </HeadingContainer>
+        <ContentContainer>
+            <LeftContentContainer>
+                <FlushTop>
+                    <Centralised align = 'center'>
+                        <DescriptionBox >
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod neque nec accumsan pulvinar. Integer suscipit leo sit amet venenatis iaculis. Fusce condimentum massa nec mauris pellentesque, sit amet placerat felis maximus. Nam eu ipsum maximus, dapibus mi vitae, tristique turpis. Suspendisse ornare nunc tortor, a elementum ligula euismod ultricies. Donec interdum dolor ut fringilla molestie. Sed ornare sapien in urna porta volutpat. Cras varius sapien quis leo tempor blandit. Nam ut malesuada tellus. Donec semper rhoncus nisl, vitae tincidunt libero efficitur eget. In laoreet, ligula a blandit porttitor, purus sapien condimentum arcu, maximus porttitor mi diam ut libero.Aliquam dapibus consectetur dolor eu sodales. Nulla rhoncus justo et ipsum porta, eget hendrerit arcu porta. Pellentesque tincidunt dolor ut magna malesuada, vitae blandit neque dictum.
+                         </DescriptionBox>
+                    </Centralised>
+                </FlushTop>
+            </LeftContentContainer>
+
+            <RightContentContainer>
+                <FlushTop>
+                    <Centralised>
+                    <TopImage src = 'gibbresh.png' />
+                    <MiddleImage src = 'gibbresh.png'/> 
+                    <BottomImage src = 'gibbresh.png'/>
+                    </Centralised>
+                </FlushTop>
+            </RightContentContainer>
+        </ContentContainer>
     </AboutContainer>
 
   )
--- components/About/styles.tsx
+++ components/About/styles.tsx
@@ -3,16 +3,122 @@ import {Flex, Box, Heading, Image, chakra} from '@chakra-ui/react'
 export const AboutContainer = chakra(Flex, {
     baseStyle : {
         w : '100%',
+        h : '100vh',
+        flexDirection : 'column',
+        align : 'center',
+        justifyContent : 'center',
+        paddingTop : {base : '15%', md : '5%'}
+    }, 
+})
+
+export const HeadingContainer = chakra(Flex, {
+    baseStyle : {
+        w : '100%',
+        h : '15%',
+        align : 'center',
+        justifyContent : 'center',
+        padding : '5%',
+        paddingBottom : '7%',
+    }
+})
+
+export const AboutHeading = chakra(Heading, {
+    baseStyle : {
+        fontSize : {base : 'xl', md : '4xl'}
+    },
+})
+
+export const ContentContainer = chakra(Flex, {
+    baseStyle : {
+        w : '100%',
+        h : '85%',
+        direction : {base : 'column-reverse', md :'row'},
+        align : 'center',
+    }
+})
+
+export const LeftContentContainer = chakra(Flex, {
+    baseStyle : {
+        w : {base:'80%', md:'65%'},
         h : '100%',
         direction : 'column',
         align : 'center',
-        justify : 'center',
-        paddingTop : '10%'
-    }, 
-    sizes: {
-        md: {
-          paddingTop : '2.5%'
-        },
-      },
+    }
+})
+
+export const FlushTop = chakra(Flex, {
+    baseStyle : {
+        height : '50%', 
+        align : 'center', 
+        justifyContent : 'center',
+    }
+})
+
+export const Centralised = chakra(Flex, {
+    baseStyle : {
+        justifyContent : 'center',
+        align : 'center',
+    }
+})
+
+export const DescriptionBox = chakra(Box, {
+    baseStyle : {
+        w : '90%',
+        display : 'flex-column',
+        borderWidth : '1px',
+        borderRadius : '20',
+        padding : '20px 10px',
+        boxShadow : '0px 2px 1px grey',
+        backdropFilter :'blur(6px)',
+        color : '#001158'
+    } 
+})
+
+export const RightContentContainer = chakra(Flex, {
+    baseStyles : {
+        direction : 'column',
+        align : 'center', 
+        justify : {base :'center', md : 'start'}, 
+        w : {base:'80%', md:'30%'}, 
+        h : {base:'50%',md:'100%'}, 
+        padding : {base : '20% 0', md :'0'},
+    }
+})
+
+export const TopImage = chakra(Image, {
+    baseStyle : { 
+        position : 'relative', 
+        zIndex : '2', 
+        left : '1rem',
+        right : {base :'13rem', md :'13rem'}, 
+        bottom : {base : '7rem',md : '0rem'},
+        borderRadius : '5px',
+        height : '6rem',
+        width : '10rem',
+    }
+})
+
+export const MiddleImage = chakra(Image, {
+    baseStyle : { 
+        position : 'relative', 
+        zIndex : '1', 
+        left : {base :'2rem', md :'-4rem'},
+        bottom : {base :'1rem', md :'-3rem'},
+        borderRadius : '5px',
+        height : '12rem',
+        width : '15rem',
+    }
+})
+
+export const BottomImage = chakra(Image, {
+    baseStyle : { 
+        position : 'relative', 
+        zIndex : '2', 
+        left : {base :'13rem', md :'-25rem'},
+        top : {base : '2rem',md : '9rem'},
+        borderRadius : '5px',
+        height : '9rem',
+        width : '11rem',
+    }
 })
 
