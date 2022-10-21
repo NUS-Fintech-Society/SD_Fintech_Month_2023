fa66cb793d7c1eca2ab2f489a88de6017a98deb6
Zheng Yu <zhengyu@Zhengs-MacBook-Air.local>
2022-10-21 11:45:28 +0800

    removed about page from pages

--- pages/about.tsx
+++
@@ -1,9 +0,0 @@
-import type { NextPage } from 'next'
-import styles from '../styles/Home.module.css'
-import About from '../components/About';
-
-const AboutPage: NextPage = () => {
-    return <About/>
-}
-
-export default AboutPage;
