fea3fb3e0df94d5693e1be02985f55130dd6219f
Zheng Yu <zhengyu@r-98-107-25-172.comp.nus.edu.sg>
2022-11-30 14:48:48 +0800

    changed index page to same as develop

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,38 +2,7 @@ import type { NextPage } from 'next';
 import IndexMain from '../components/IndexMain/IndexMain';
 
 const Home: NextPage = () => {
-  const [email, setEmail] = useState<string>('');
-
-  function setEmailValue(e: ChangeEvent<HTMLInputElement>) {
-    setEmail(e.target.value);
-  }
-
-  function submitEmail() {
-    const data = { email };
-
-    fetch('/api/sheets', {
-      method: 'POST',
-      body: JSON.stringify(data),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-  }
-
-  return (
-    <div className={styles.container}>
-      <div style={{ paddingTop: '100px' }}>
-        <Input
-          placeholder="Basic usage"
-          value={email}
-          onChange={setEmailValue}
-        />
-        <Button colorScheme="blue" onClick={submitEmail}>
-          Submit
-        </Button>
-      </div>
-    </div>
-  );
+  return <IndexMain />;
 };
 
 export default Home;
