7b5c890dfba76c3493ef0ca517814be0c3d6e135
Jiwei99 <jwlim99@gmail.com>
2022-12-08 23:39:53 +0800

    Add contact integration

--- utils/GoogleSheetsUtil.ts
+++ utils/GoogleSheetsUtil.ts
@@ -4,17 +4,34 @@ import { getGoogleAuthClient } from './GoogleAuthUtil';
 const sheetId = process.env.GOOGLE_SHEETS_SHEET_ID;
 const sheets_v4 = sheets('v4');
 
-export async function writeToSheet(value: string) {
+export async function writeToSheet(value: string, range?: string) {
   const authToken = await getGoogleAuthClient();
 
   const result = await sheets_v4.spreadsheets.values.append({
     spreadsheetId: process.env.GOOGLE_SHEETS_SHEET_ID,
-    range: process.env.GOOGLE_SHEETS_SHEET_NAME,
+    range,
     auth: authToken,
     valueInputOption: 'USER_ENTERED',
     requestBody: {
       majorDimension: 'ROWS',
       values: [[value]],
+    },
+  });
+  console.log('Successfully written data to Sheets');
+  return result;
+}
+
+export async function writeValuesToSheet(values: string[], range?: string) {
+  const authToken = await getGoogleAuthClient();
+
+  const result = await sheets_v4.spreadsheets.values.append({
+    spreadsheetId: process.env.GOOGLE_SHEETS_SHEET_ID,
+    range,
+    auth: authToken,
+    valueInputOption: 'USER_ENTERED',
+    requestBody: {
+      majorDimension: 'ROWS',
+      values: [values],
     },
   });
   console.log('Successfully written data to Sheets');
