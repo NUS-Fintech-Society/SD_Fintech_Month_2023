d6139ad6407ac30745121af203f13a67e1318cb6
Junx <junxiongong2@gmail.com>
2022-12-15 16:45:16 +0100

    refactor images to use next/image

--- components/About/index.tsx
+++ components/About/index.tsx
@@ -1,33 +1,34 @@
 import { Img } from '@chakra-ui/react';
+import Image from 'next/image';
+import BubbleEffect2 from '../../public/BubbleEffect2.png';
+import AboutImage1 from '../../public/AboutImage1.png';
+import AboutImage3 from '../../public/AboutImage3.png';
+import AboutImage2 from '../../public/AboutImage2.png';
 
 function About() {
   return (
     <div className="lg:h-screen min-h-screen flex flex-col items-center justify-center relative">
-      <Img
-        src="BubbleEffect2.png"
-        className="h-full -z-10 absolute object-cover"
-        placeholder=""
+      <Image
+        src={BubbleEffect2}
+        objectFit="cover"
+        layout="fill"
+        className="h-screen w-full -z-10 absolute"
+        alt="background image 3"
       />
       <div className="text-xl md:text-4xl text-[#001158] font-bold mb-6">
         ABOUT FINTECH MONTH
       </div>
       <div className="flex flex-col lg:flex-row-reverse items-center justify-center relative lg:mx-16">
         <div className="grid grid-cols-12 grid-rows-[12] w-2/3 lg:w-1/2">
-          <Img
-            src="AboutImage1.png"
-            objectFit="cover"
-            className="col-start-3 col-span-6 z-10 row-span-2 row-start-1"
-          />
-          <Img
-            src="AboutImage3.png"
-            objectFit="cover"
-            className="col-start-5 col-span-8 z-0 row-span-3 row-start-2"
-          />
-          <Img
-            src="AboutImage2.png"
-            objectFit="cover"
-            className="col-start-2 col-span-6 z-10 row-span-2 row-start-4"
-          />
+          <div className="col-start-3 col-span-6 z-10 row-span-2 row-start-1">
+            <Image src={AboutImage1} objectFit="cover" alt="pic 1" />
+          </div>
+          <div className="col-start-5 col-span-8 z-0 row-span-3 row-start-2">
+            <Image src={AboutImage3} objectFit="cover" alt="pic 3" />
+          </div>
+          <div className="col-start-2 col-span-6 z-10 row-span-2 row-start-4">
+            <Image src={AboutImage2} objectFit="cover" alt="pic 2" />
+          </div>
         </div>
         <div className="flex shadow-xl rounded-3xl p-4 md:p-6  backdrop-blur-sm bg-[#ffffffcf] my-4 w-3/4 lg:w-1/2 ">
           <div className="text-[#001158]">
