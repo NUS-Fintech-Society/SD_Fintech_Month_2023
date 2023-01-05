b407fb241277b372474c0d0e0c6d665d34c147b6
Zheng Yu <zhengyu@zhengs-air.lan>
2023-01-05 23:01:10 +0800

    created open,close,results state and changed logo to say weekly quizzes

--- data/LeaderBoard.ts
+++ data/LeaderBoard.ts
@@ -1,64 +1,70 @@
 import { LeaderBoardMember } from '../types/LeaderboardMember';
 
-export const LeaderBoardData: LeaderBoardMember[] = [
+export const QuizWinners: LeaderBoardMember[] = [
   {
     rank: 1,
     username: 'Apple',
     accuracy: 100,
     time: 12.0,
+    email: 'apple@gmail.com',
   },
   {
     rank: 2,
     username: 'Apple',
     accuracy: 100,
     time: 12.0,
+    email: 'apple@gmail.com',
   },
   {
     rank: 3,
     username: 'Apple',
     accuracy: 100,
     time: 12.0,
+    email: 'apple@gmail.com',
   },
   {
     rank: 4,
     username: 'Apple',
     accuracy: 100,
     time: 12.0,
+    email: 'apple@gmail.com',
   },
   {
     rank: 5,
     username: 'Apple',
     accuracy: 100,
     time: 12.0,
+    email: 'apple@gmail.com',
   },
+];
+
+export const LuckyDrawWinners: LeaderBoardMember[] = [
   {
-    rank: 6,
+    rank: 1,
     username: 'Apple',
     accuracy: 100,
     time: 12.0,
+    email: 'apple@gmail.com',
   },
   {
-    rank: 7,
+    rank: 2,
     username: 'Apple',
     accuracy: 100,
     time: 12.0,
+    email: 'apple@gmail.com',
   },
   {
-    rank: 8,
+    rank: 3,
     username: 'Apple',
     accuracy: 100,
     time: 12.0,
+    email: 'apple@gmail.com',
   },
   {
-    rank: 9,
+    rank: 4,
     username: 'Apple',
     accuracy: 100,
     time: 12.0,
-  },
-  {
-    rank: 10,
-    username: 'Apple',
-    accuracy: 100,
-    time: 12.0,
+    email: 'apple@gmail.com',
   },
 ];
