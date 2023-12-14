c3a77a0708525a9897132048692fc002132efb47
= <yeohchongyics@gmail.com>
2023-12-14 17:47:30 +0800

    Change Footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import SocialMediaIcons from '../SocialMediaIcons';
-import { FooterContainer } from './styles';
+import { FooterContainer, FooterLeftContainer, VerticalLine } from './styles';
+import NextLink from 'next/link';
+import { Routes } from '../../constants/Routes.enum';
+import { Image, Link } from '@chakra-ui/react';
 
 export default function Footer() {
+  <span style={{ marginLeft: '5px', marginRight: '5px' }}>|</span>;
   return (
     <FooterContainer>
-      <SocialMediaIcons color="white" />
+      <FooterLeftContainer>
+        <SocialMediaIcons color="white" />
+        <VerticalLine>|</VerticalLine>© 2023 NUS FINTECH SOCIETY
+        <VerticalLine>|</VerticalLine>
+        ALL RIGHTS RESERVED
+      </FooterLeftContainer>
+      <NextLink href={Routes.HOME} passHref>
+        <Link>
+          <Image src="/fintechsoc-logo.png" w="150px" minWidth="150px" alt="" />
+        </Link>
+      </NextLink>
     </FooterContainer>
   );
 }
--- components/Footer/styles.tsx
+++ components/Footer/styles.tsx
@@ -2,12 +2,29 @@ import { Box, chakra } from '@chakra-ui/react';
 
 export const FooterContainer = chakra(Box, {
   baseStyle: {
+    paddingLeft: '30px',
+    paddingRight: '30px',
     display: 'flex',
+    justifyContent: 'space-between',
     width: '100%',
     height: '60px',
     background: 'brand.primary',
     alignContent: 'center',
-    justifyContent: 'flex-end',
-    paddingRight: '10',
+    color: 'white',
+  },
+});
+export const FooterLeftContainer = chakra(Box, {
+  baseStyle: {
+    display: 'flex',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+});
+
+// export the style <span style={{marginLeft: "5px", marginRight: "5px"}}>|</span>
+export const VerticalLine = chakra(Box, {
+  baseStyle: {
+    marginLeft: '10px',
+    marginRight: '10px',
   },
 });
