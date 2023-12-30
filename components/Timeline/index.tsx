64fb103bf235c659a12ee6d1fdb879d928b321c6
9teMare <wang.zihao@u.nus.edu>
2023-12-30 14:46:31 +0800

    bring back react-chrono

--- components/Timeline/index.tsx
+++ components/Timeline/index.tsx
@@ -1,20 +1,22 @@
-// import React from "react";
-// import { Chrono } from "react-chrono";
+import React from 'react';
+import { Chrono } from 'react-chrono';
 
-// export default function Timeline() {
-//     const items = [{
-//         title: "May 1940",
-//         cardTitle: "Dunkirk",
-//         cardSubtitle: "Men of the British Expeditionary Force (BEF)",
-//         cardDetailedText: "Men",
-//         media: {
-//             type: "IMAGE",
-//         }
-//     }]
+export default function Timeline() {
+  const items = [
+    {
+      title: 'May 1940',
+      cardTitle: 'Dunkirk',
+      cardSubtitle: 'Men of the British Expeditionary Force (BEF)',
+      cardDetailedText: 'Men',
+      media: {
+        type: 'IMAGE',
+      },
+    },
+  ];
 
-//     return (
-//         <div style={{ width: "500px", height: "2000px"}}>
-//             <Chrono items={items}  />
-//         </div>
-//     )
-// }
+  return (
+    <div style={{ width: '500px', height: '2000px' }}>
+      <Chrono items={items} />
+    </div>
+  );
+}
