6b298149c6749e9ac68f55f4ca04201186f54056
pyhaw <peiyihaww@gmail.com>
2023-12-30 13:59:01 +0800

    fix: fix tickets 1, 2, 3, 5, 6, 7, 8, 9, 11, 12

--- components/RegisterButton/index.tsx
+++ components/RegisterButton/index.tsx
@@ -10,7 +10,7 @@ export default function RegisterButton() {
       <button className="bg-[#67C3C6] relative rounded-lg py-2 px-20">
         <div className="flex flex-col gap-1 text-black font-bold ">
           <div className="text-base">Register Now!</div>
-          <div className="text-xs underline">By 01 Jan 2024</div>
+          <div className="text-xs underline">By 03 Jan 2024 3pm</div>
         </div>
       </button>
     </a>
